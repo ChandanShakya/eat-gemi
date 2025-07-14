5033243d736dd2a695a24ca96209b2be093ae757
Chandan Shakya <chandanshakya@email.com>
2025-07-14 16:59:09 +0545

    Add backend laravel basic files and git ignores

---
+++ backend/routes/auth.php
@@ -0,0 +1,6 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+// Auth routes will be defined here when using Laravel Breeze or similar
+// For now, we'll keep this file empty as we're using Sanctum with custom controllers
